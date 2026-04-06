538073a07f40890a99a8b128b21b5fb24c245766
LakshitaAgg15 <lakshitaaggarwal15@gmail.com>
2026-04-06 18:45:56 +0530

    added more fitness quests

--- src/data/creativeQuests.ts
+++ src/data/creativeQuests.ts
@@ -9,4 +9,5 @@ export const creativeQuests: Quest[] = [
   { id: "c3", title: "Create a short story in under 5 sentences" },
   { id: "c4", title: "Make a playlist for a specific vibe " },
   { id: "c5", title: "Take a creative photo" }
+  
 ];
